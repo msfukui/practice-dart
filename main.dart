e89a16e21ae88a0af8c5d890de0d9f9a8074b3ca
msfukui <msfukui@gmail.com>
2023-07-07 11:55:36 +0900

    Impl Async

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
-void main() {
+import 'dart:io';
+
+void main() async {
   // Hello World
   print('Hello, World!');
 
@@ -60,6 +62,14 @@ void main() {
   // Interfaces and abstract classes
   var mock = MockSpaceship();
   mock.describeWithEmphasis();
+
+  // Async
+  printWithDelay('Async sample message.');
+  createDescriptions(['a','b','c']);
+  await for (var m in report(voyager, ['a','b','c'])) {
+    print(m);
+  }
+  deleteDescriptions(['a','b','c']);
 }
 
 // Functions
@@ -155,5 +165,44 @@ abstract class Describable {
     print('=========');
     describe();
     print('=========');
+  }
+}
+
+// Async
+const oneSecond = Duration(seconds: 1);
+// ...
+Future<void> printWithDelay(String message) async {
+  await Future.delayed(oneSecond);
+  print(message);
+}
+
+Future<void> createDescriptions(Iterable<String> objects) async {
+  for (final object in objects) {
+    try {
+      var file = File('$object.txt');
+      if (await file.exists()) {
+        var modified = await file.lastModified();
+        print('File for $object already exists. It was modified on $modified.');
+        continue;
+      }
+      await file.create();
+      await file.writeAsString('Start describing $object in this file.');
+    } on IOException catch (e) {
+      print('Cannot create description for $object: $e');
+    }
+  }
+}
+
+Stream<String> report(Spacecraft craft, Iterable<String> objects) async* {
+  for (final object in objects) {
+    await Future.delayed(oneSecond);
+    yield '${craft.name} flies by $object';
+  }
+}
+
+void deleteDescriptions(Iterable<String> objects) {
+  for (final object in objects) {
+    var file = File('$object.txt');
+    file.deleteSync();
   }
 }
